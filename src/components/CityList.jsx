a1ff4780b5753ae11824e2954526b3d4e69e0d8c
setbs <kr9ka41@gmail.com>
2026-01-13 04:10:30 +0100

    City Search

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -3,19 +3,30 @@ import { Link } from "react-router-dom";
 
 function CityList({ onSelect }) {
   const cities = ["Warszawa", "Wrocław", "Kraków", "Gdańsk", "Poznań"];
-  
+  const [query, setQuery] = useState("");
+
+  const filteredCities = cities.filter((city) =>
+    city.toLowerCase().includes(query.toLowerCase())
+  );
   return (
-    <ul>
-      {cities.map((city) => (
-        <li
-          key={city}
-          style={{ cursor: "pointer" }}
-          onClick={() => onSelect(city)}
-        >
-          <Link to={`/city/${city}`}>{city}</Link> 
-        </li>
-      ))}
-    </ul>
+    <div>
+      <input 
+        type="text" 
+        placeholder="Find your city..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+
+      <ul>
+        {filteredCities.map((city) => (
+          <li key={city}>
+            <Link to={`/city/${city}`}>{city}</Link>
+          </li>
+        ))}
+
+        {filteredCities.lenght === 0 && <p> Nothing found 👾</p>}
+      </ul>
+    </div>
   );
 }
 
