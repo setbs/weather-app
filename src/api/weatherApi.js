bc84bc67ed39c5b6218971a04febf81070a6971c
setbs <kr9ka41@gmail.com>
2026-01-13 07:11:51 +0100

    api fixv2

--- src/api/weatherApi.js
+++ src/api/weatherApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_KEY = "77f5a51fc6b3738d14b7d05b978e55be";
+const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
 
 export const getWeatherByCity = async (city) => {
   const response = await axios.get(
